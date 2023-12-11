47e5edeefdcb5c012b6161fec56c22d8708f3b75
Dark <fedefunes90@gmail.com>
2023-12-11 13:56:25 -0300

    edit

--- src/cogs/verification.py
+++ src/cogs/verification.py
@@ -157,9 +157,46 @@ class DeleteRobloxAccountView(discord.ui.View):
         await interaction.response.edit_message(embed=discord.Embed(description="<:checked:1173356058387951626> Successfully deleted your Roblox info.", color=discord.Color.green()), view=None)
 
 
+class VerifyViewPersistent(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.Button(label="Verify", emoji="<:link:986648044525199390>", custom_id="verifypersistent")
+    async def verify_persistent_callback(self, button, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        roblox_data = await get_roblox_info(interaction.user.id)
+        if roblox_data:
+            username = roblox_data["data"]["name"]
+            avatar_url = roblox_data["data"]["avatar"]
+            display_name = roblox_data["data"]["displayName"]
+            roblox_id = roblox_data["data"]["id"]
+            description = roblox_data["data"]["description"]
+
+            embed = discord.Embed(
+                title=f":wave: Hello there, {username}!", color=discord.Color.nitro_pink())
+            embed.set_thumbnail(url=avatar_url)
+
+            embed.add_field(name="Display Name", value=display_name)
+            embed.add_field(name="Roblox ID", value=roblox_id)
+            embed.add_field(name="Description", value=description)
+
+            if roblox_data["blacklisted"]:
+                embed.add_field(name="Blacklist Info",
+                                value=str(roblox_data["message"]))
+
+            embed.description = "You are verified! If you wish to link another account, first delete your linked account using the button below and run this command again."
+            return await interaction.followup.send(embed=embed, view=DeleteRobloxAccountView(interaction.user), ephemeral=True)
+
+        await interaction.followup.send(content=":wave: To verify click the button below and follow the steps.", view=VerifyView(interaction.user), ephemeral=True)
+
+
 class Verification(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.bot.add_view(VerifyViewPersistent())
 
     @commands.slash_command(description="Verify or delete your verified account with Sally")
     async def verify(self, ctx: discord.ApplicationContext):
@@ -250,7 +287,33 @@ class Verification(commands.Cog):
 
         data["avatar"] = avatar_url
         await add_roblox_info(user_id, roblox_id, data)
+        member = ctx.guild.get_member(int(user_id))
+        try:
+            nickname = f"{data['displayName']} (@{data['name']})"
+            if len(nickname) > 32:
+                nickname = f"{data['name']}"
+
+            await member.edit(nick=nickname)
+        except:
+            pass
+
+        try:
+            verified_role = ctx.guild.get_role(1183609826002079855)
+            await member.add_roles(verified_role, reason=f"Verified account as: {nickname}")
+        except:
+            pass
+
         await ctx.send(embed=discord.Embed(description=f"<:checked:1173356058387951626> Successfully forced verification on <@{user_id}> as Roblox account `{roblox_id}`", color=discord.Color.green()))
+
+    @commands.command(name="verifymsg")
+    async def verify_message(self, ctx: commands.Context, channel: discord.TextChannel):
+        embed = discord.Embed(
+            title="<:link:986648044525199390> Verfication Required!", color=discord.Color.nitro_pink())
+        embed.description = "You are required to verify your Roblox account to be able to attend to our **INKIGAYO** shows. If you are not verified, you will not be able to join our Roblox game and be part of the audience.\n\n<:info:881973831974154250> All data stored is public information about your Roblox account. You can delete it at any time by clicking the button again or by using </verify:1183583727473917962> in a channel."
+
+        embed.set_footer(text="If you run into any issues, please DM Dark")
+        await channel.send(embed=embed, view=VerifyViewPersistent())
+        await ctx.send(content="Sent!")
 
 
 def setup(bot):
