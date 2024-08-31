c71355e5090bacab000b587ba633fa34db737540
Dark <fedefunes90@gmail.com>
2024-08-31 14:03:32 -0300

    chore: Update discord.option descriptions in verification.py

--- src/cogs/verification.py
+++ src/cogs/verification.py
@@ -412,8 +412,8 @@ class Verification(commands.Cog):
 
     @commands.slash_command(description="Get someones Roblox information", contexts={discord.InteractionContextType.guild})
     @commands.guild_only()
-    @discord.option("user", discord.Member, "The user to get the info from", default=None)
-    @discord.option("roblox_id", "The Roblox ID to look up for", default=None)
+    @discord.option("user", discord.Member, description="The user to get the info from", default=None)
+    @discord.option("roblox_id", str, description="The Roblox ID to look up for", default=None)
     async def getinfo(self, ctx: discord.ApplicationContext, user: discord.Member, roblox_id: str):  # type: ignore
         if user and roblox_id:
             await ctx.respond(embed=discord.Embed(description=f"{aesthetic.Emojis.error} You have to either provide a `user` **or** `roblox_id`.", color=aesthetic.Colors.error))
